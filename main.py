37ce704308b81e6edaeddde4871c203d1dde1fa9
fss4 <f.spencer.stubbs@gmail.com>
2026-03-04 18:51:34 -0500

    added a moveable player sprite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,14 +10,23 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,  drawable)
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for grp in drawable:    
+            grp.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
+    
 if __name__ == "__main__":
     main()
